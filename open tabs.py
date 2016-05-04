6e2d8d1834463fbd174514c09f4a83618af81c30
Aradhya Makkar <aradhyamakkar97@gmail.com>
2016-05-04 19:29:17 +0530

    Added files via upload

---
+++ open tabs.py
@@ -0,0 +1,14 @@
+import webbrowser,sys
+
+# expand this!!!!
+if sys.argv[1]=='social':
+    webbrowser.open('https://www.facebook.com/')
+    webbrowser.open('https://www.twitter.com/')
+    webbrowser.open('https://www.quora.com/')
+
+if sys.argv[1]=='read':
+    webbrowser.open('https://news.ycombinator.com/')
+    webbrowser.open('http://waitbutwhy.com/')
+    webbrowser.open('https://medium.com/')
+    webbrowser.open('http://thenextweb.com/')
+    webbrowser.open('http://yourstory.com/')
